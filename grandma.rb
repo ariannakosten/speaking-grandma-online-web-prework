5d506ab70f99fa936c43a9da676bf6422a6acbd5
Arianna Kosten <arianna.kosten@gmail.com>
2019-04-13 22:05:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(phrase)
     "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "Hi!"
     "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "WHAT?"
+  elsif phrase == "WHAT DID YOU EAT TODAY?"
     "NO, NOT SINCE 1938!"
   end
 end
