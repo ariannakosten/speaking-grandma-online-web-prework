d6451e9b04dccb1db6efada52f26099965a17843
Arianna Kosten <arianna.kosten@gmail.com>
2019-04-13 21:59:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,8 +3,7 @@ def speak_to_grandma(phrase)
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == "Hi Nana, how are you?"
     "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "H
-  i!"
+  elsif phrase == "Hi!"
     "HUH?! SPEAK UP, SONNY!"
   end
 end
