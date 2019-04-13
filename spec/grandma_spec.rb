56c29866d7a8620bf25df025132fadab20cbd77e
Arianna Kosten <arianna.kosten@gmail.com>
2019-04-13 22:17:48 +0000

    Automatically backed up by Learn

--- spec/grandma_spec.rb
+++ spec/grandma_spec.rb
@@ -22,7 +22,9 @@ describe '#speak_to_grandma' do
   it 'responds with Im tired leave me alone when you say anything else ' do
     expect(speak_to_grandma('Im hungry')).to eq "IM tired leave me alone"
     
-    expect(speak_to_grandma('I want to play)ired leave me alone"
+    expect(speak_to_grandma('I want to play!').to eq "ave me alone"
   end
 
 end
+
+
